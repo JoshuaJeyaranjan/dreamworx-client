7b538451cc3f20392837e2f18cfbbc8e42458913
Joshua Jeyaranjan <joshuajeyaranjan@macbook-pro-5.lan>
2024-07-09 14:56:19 -0400

    add contact components to contact page

--- src/pages/ContactPage/ContactPage.jsx
+++ src/pages/ContactPage/ContactPage.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 import './ContactPage.scss'
+import Nav from '../../components/Nav/Nav'
+import Footer from '../../components/Footer/Footer'
+import ContactForm from '../../components/ContactForm/ContactForm'
+import ContactInfo from '../../components/ContactInfo/ContactInfo'
 
 export default function ContactPage() {
   return (
-    <div>ContactPage</div>
+    <div className='contact-page'>
+      <Nav/>
+      <h1 className='contact-page__header' >Contact Us Today!</h1>
+      <ContactForm/>
+      <ContactInfo/>
+      <Footer/>
+    </div>
   )
 }
