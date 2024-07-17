83f5809ba47ae41735ded840a43ad6170e5dec83
Joshua Jeyaranjan <joshuajeyaranjan@macbook-pro-5.lan>
2024-07-17 13:25:19 -0400

    test

---
+++ src/components/GlitchBody/GlitchBody 3.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import'./GlitchBody.scss'
+
+export default function GlitchBody() {
+  return (
+    <section className='glitch-body'>
+    <div className='glitch__text-container'>
+       <h3 className='glitch__sub-header'>ENTRUST YOUR VEHICLE'S</h3>
+       <h2 className='glitch__header' data-text='RESTORATION TO OUR BODY'>RESTORATION TO OUR BODY SHOP EXPERTS</h2>
+       
+       <p className='glitch__paragraph'>From dent repairs to full-body painting and innovative options like peelable and smart paint, we offer comprehensive body care solutions. Our technicians specialize in chassis collision repairs, electric repairs, and all aspects of body maintenance. At our Body Shop, we are committed to restoring your vehicle’s structural integrity and aesthetics.</p>
+    </div>
+
+    </section>
+  )
+}
---
+++ src/components/ImageCarousel/ImageCarousel 3.jsx
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import './ImageCarousel.scss';
+
+const images = [
+  'src/assets/images/cool-car-1.jpg',
+  'src/assets/images/cool-car-2.jpg',
+  'src/assets/images/cool-car-3.jpg',
+  'src/assets/images/cool-car-4.jpg',
+  'src/assets/images/cool-car-5.jpg',
+  'src/assets/images/cool-car-6.jpg',
+  'src/assets/images/cool-car-7.jpg',
+  'src/assets/images/cool-car-8.jpg',
+];
+
+export default function ImageCarousel() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex(prevIndex => (prevIndex + 1) % images.length);
+    }, 3000); // Change image every 3 seconds
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className='image-carousel'>
+      <img src={images[currentIndex]} alt={`Slide ${currentIndex + 1}`} className='image-carousel__image' />
+    </div>
+  );
+}
